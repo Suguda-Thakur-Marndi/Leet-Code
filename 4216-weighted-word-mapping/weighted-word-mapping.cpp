78dfc37bbf2124a6bb880c13a99ec11a60240513
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-07-09 15:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (97.37%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (const string& word : words) {
+            long long sum = 0;
+
+            for (char c : word) {
+                sum += weights[c - 'a'];
+            }
+
+            ans.push_back('z' - (sum % 26));
+        }
+
+        return ans;
+    }
+};
