1a61bbc1b481703490a5bd0199fb282c9fff2c15
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-07-15 16:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (41.95%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        
+        vector<vector<int>> arr(n, vector<int>(n));
+
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
+
+        int num = 1;
+
+        while(top <= bottom && left <= right) {
+
+   
+            for(int i = left; i <= right; i++) {
+                arr[top][i] = num++;
+            }
+            top++;
+
+ 
+            for(int i = top; i <= bottom; i++) {
+                arr[i][right] = num++;
+            }
+            right--;
+
+            for(int i = right; i >= left; i--) {
+                arr[bottom][i] = num++;
+            }
+            bottom--;
+
+            for(int i = bottom; i >= top; i--) {
+                arr[i][left] = num++;
+            }
+            left++;
+        }
+
+        return arr;
+    }
+};
