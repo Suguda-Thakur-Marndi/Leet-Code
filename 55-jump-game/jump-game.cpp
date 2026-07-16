93bd0fc9f81ff492238c86fbc1ed770c311e075c
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-07-17 00:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (58.70%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int mx=0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(i>mx){
+                return false;
+            }
+
+            mx=max(mx,i+nums[i]);
+              if(mx >= nums.size() - 1) {
+                return true;
+            }
+        }
+
+        return true;
+        
+    }
+};
