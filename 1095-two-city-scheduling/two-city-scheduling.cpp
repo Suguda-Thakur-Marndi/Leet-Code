6f88e008be845c6086d7c2a5cf1d0589d742c773
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-07-22 02:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (82.22%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(),
+    [](vector<int>& a, vector<int>& b) {
+        return (a[0] - a[1]) < (b[0] - b[1]);
+    });
+    int cost =0;
+    int n=costs.size();
+    for(int i=0;i<n/2;i++){
+        cost+=costs[i][0];
+    }
+      for(int i=n/2;i<n;i++){
+        cost+=costs[i][1];
+    }
+
+    return cost;
+     
+    }
+};
