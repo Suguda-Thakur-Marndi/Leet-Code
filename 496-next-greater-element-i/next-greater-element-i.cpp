df5d6b44f68863bbe228b8de1408abcf9900eb93
Cyber <sugudathakurmarndi1106@gmail.com>
2026-07-29 23:45:51 +0530

    Time: 1 ms (53.84%) | Memory: 12.8 MB (40.01%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> mp;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            mp[nums2[i]] = st.empty() ? -1 : st.top();
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for (int x : nums1) {
+            if (mp.find(x) != mp.end())
+                ans.push_back(mp[x]);
+            else
+                ans.push_back(-1);
+        }
+
+        return ans;
+    }
+};
