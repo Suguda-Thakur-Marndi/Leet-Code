8359b40adb3aabe016b6527e11db8f4e6f6c0a3b
Cyber <sugudathakurmarndi1106@gmail.com>
2026-07-29 23:41:32 +0530

    Time: 7 ms (39.69%) | Memory: 10.6 MB (24.65%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
+
+        unordered_map<char,int> m1, m2;
+        for(char c : s1) m1[c]++;
+
+        int k = s1.size();
+
+        for(int i = 0; i < s2.size(); i++) {
+            m2[s2[i]]++;
+            if(i >= k) {
+                m2[s2[i - k]]--;
+                if(m2[s2[i - k]] == 0)
+                    m2.erase(s2[i - k]);
+            }
+
+            if(m1 == m2) return true;
+        }
+
+        return false;
+    }
+};
